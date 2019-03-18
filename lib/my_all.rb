9c6162cc6f44f73251debe37b5b3d06ed7f892f3
Ryan Sise <kerneltux0@gmail.com>
2019-03-18 20:07:43 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,11 @@
 def my_all?(collection)
 	counter = 0
 	while counter < collection.length
-	  yield(collection[counter]) if block_given?
+	  block_return_values << yield(collection[counter])
 		counter += 1
 	end
 end
 
-my_all?([1,2,3]) {|i| i < 2}
+def block_return_values
+
+end
