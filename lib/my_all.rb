6a8d9952aa196ec1ed00a5f437a94ef0a7f6d20e
Ryan Sise <kerneltux0@gmail.com>
2019-03-18 20:10:01 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,11 +1,13 @@
 def my_all?(collection)
 	counter = 0
+	block_return_values = []
 	while counter < collection.length
 	  block_return_values << yield(collection[counter])
 		counter += 1
 	end
-end
-
-def block_return_values
-
+	if block_return_values.include?(false)
+		false
+	else
+		true
+	end
 end
